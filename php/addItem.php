6470c8288758a5d65bbb3066ee0296a74bc4fe33
Mathieu Girard <mat.girard@gmail.com>
2016-01-10 13:29:37 -0500

    ajout php'

---
+++ php/addItem.php
@@ -0,0 +1,17 @@
+<?php
+    //open connection to mysql db
+    $connection = mysqli_connect("localhost","root","enirak","scrap") or die("Error " . mysqli_error($connection));
+
+    $nom = $_GET['nom'];
+    $compagnie = $_GET['compagnie'];
+    $prix = intval($_GET['prix']);
+    $quantite = intval($_GET['quantite']);
+
+
+	mysqli_query($connection,"INSERT INTO items(nom, prix, quantite, compagnie) VALUES ('$nom', $prix, $quantite, '$compagnie')");
+
+
+
+
+    mysqli_close($connection);
+?>
